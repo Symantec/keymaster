5dbde3b9f4c8d3d7ce48be7841c8eb9fdeabca33
Camilo Viecco <camilo_viecco1@symantec.com>
2017-01-07 02:00:23 +0000

    adding more tests

--- cmd/getcreds/main_test.go
+++ cmd/getcreds/main_test.go
@@ -7,8 +7,12 @@ import (
 	"testing"
 )
 
+const simpleValidConfigFile = `base:
+    gen_cert_urls: "https://localhost:33443/certgen/"
+`
+
 func TestGenKeyPairSuccess(t *testing.T) {
-	tmpfile, err := ioutil.TempFile("", "example")
+	tmpfile, err := ioutil.TempFile("", "test_genKeyPair_")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -43,6 +47,36 @@ func TestGetUserHomeDirSuccess(t *testing.T) {
 	if len(homeDir) < 1 {
 		t.Fatal("invalid homedir")
 
+	}
+}
+
+func TestLoadVerifyConfigFileSuccess(t *testing.T) {
+	//content := []byte("temporary file's content")
+	tmpfile, err := ioutil.TempFile("", "test_LoadVerifyConfig")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer os.Remove(tmpfile.Name()) // clean up
+
+	if _, err := tmpfile.Write([]byte(simpleValidConfigFile)); err != nil {
+		t.Fatal(err)
+	}
+	if err := tmpfile.Close(); err != nil {
+		t.Fatal(err)
+	}
+	_, err = loadVerifyConfigFile(tmpfile.Name())
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	// TODO: validate loaded file contents
+}
+
+func TestLoadVerifyConfigFileFailNoSuchFile(t *testing.T) {
+	_, err := loadVerifyConfigFile("NonExistentFile")
+	if err == nil {
+		t.Fatal("Success on loading Nonexistent File!")
 	}
 
 }
