227c1a96186a1b2631ecc0654fecb2ae89ddd641
Camilo Viecco <camilo_viecco1@symantec.com>
2017-07-10 15:31:21 +0000

    adding per user load/save operation

--- cmd/ssh_usercert_gen/storage.go
+++ cmd/ssh_usercert_gen/storage.go
@@ -5,6 +5,7 @@ import (
 	"encoding/gob"
 	"io/ioutil"
 	"log"
+	"os"
 	"path/filepath"
 )
 
@@ -35,19 +36,41 @@ func (state *RuntimeState) LoadUserProfiles() error {
 	return decoder.Decode(&state.userProfile)
 }
 
+/// Adding api to be load/save per user
 func getFilenameForUser(state *RuntimeState, username string) string {
 	return filepath.Join(state.Config.Base.DataDirectory, userProfilePrefix+username+userProfileSuffix)
 }
 
-/// Now we change the api to be load/save per user
-func (state *RuntimeState) LoadUserProfile(username string) (*userProfile, error) {
-	userProfilePath := getFilenameForUser(state, username)
-	fileBytes, err := exitsAndCanRead(userProfilePath, "user Profile file")
+// If there a valid user profile returns: profile, true nil
+// If there is NO user profile returns nil, false, nil
+// Any other case: ni, false, error
+func (state *RuntimeState) LoadUserProfile(username string) (profile *userProfile, ok bool, err error) {
+	fileName := getFilenameForUser(state, username)
+
+	if _, err := os.Stat(fileName); os.IsNotExist(err) {
+		return nil, false, nil
+	}
+
+	fileBytes, err := exitsAndCanRead(fileName, "user Profile file")
 	if err != nil {
 		log.Printf("problem with user Profile data")
-		return nil, nil
+		return nil, false, err
 	}
 	gobReader := bytes.NewReader(fileBytes)
-	_ = gob.NewDecoder(gobReader)
-	return nil, nil
+	decoder := gob.NewDecoder(gobReader)
+	err = decoder.Decode(profile)
+	if err != nil {
+		return nil, false, err
+	}
+	return profile, true, nil
+}
+
+func (state *RuntimeState) SaveUserProfile(username string, profile *userProfile) error {
+	var gobBuffer bytes.Buffer
+	encoder := gob.NewEncoder(&gobBuffer)
+	if err := encoder.Encode(profile); err != nil {
+		return err
+	}
+	fileName := getFilenameForUser(state, username)
+	return ioutil.WriteFile(fileName, gobBuffer.Bytes(), 0640)
 }
