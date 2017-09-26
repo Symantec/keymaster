697746d1669e5fc1ffe0e6a2bd45825b2aaa46ee
Richard Gooch <rg+github0@safe-mbox.com>
2017-09-25 17:35:12 -0700

    Reorder some functions.

--- cmd/keymaster-certmond/process.go
+++ cmd/keymaster-certmond/process.go
@@ -8,16 +8,9 @@ import (
 	"os/exec"
 )
 
-func (c certCommand) processX509Cert(cert *x509.Certificate) error {
-	args := make([]string, 0, len(c.Parameters))
-	for _, template := range c.templates {
-		buffer := &bytes.Buffer{}
-		if err := template.Execute(buffer, cert); err != nil {
-			return err
-		}
-		args = append(args, buffer.String())
-	}
-	cmd := exec.Command(c.Command, args...)
+func processRawCert(command string, cert []byte) error {
+	cmd := exec.Command(command)
+	cmd.Stdin = bytes.NewBuffer(cert)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	return cmd.Run()
@@ -38,9 +31,16 @@ func (c certCommand) processSshCert(cert *ssh.Certificate) error {
 	return cmd.Run()
 }
 
-func processRawCert(command string, cert []byte) error {
-	cmd := exec.Command(command)
-	cmd.Stdin = bytes.NewBuffer(cert)
+func (c certCommand) processX509Cert(cert *x509.Certificate) error {
+	args := make([]string, 0, len(c.Parameters))
+	for _, template := range c.templates {
+		buffer := &bytes.Buffer{}
+		if err := template.Execute(buffer, cert); err != nil {
+			return err
+		}
+		args = append(args, buffer.String())
+	}
+	cmd := exec.Command(c.Command, args...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	return cmd.Run()
