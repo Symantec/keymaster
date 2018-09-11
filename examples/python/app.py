7b184d9af4dd18d7cdc50eba8029fec835aa9c7f
Jack Phelan <jack@bytesandwich.com>
2018-09-10 22:59:43 -0700

    expire/validate cookie and jwt

--- examples/python/app.py
+++ examples/python/app.py
@@ -1,7 +1,9 @@
+from datetime import datetime, timedelta
 import configparser
 import logging
 import os
 import sys
+import uuid
 
 from flask import Flask, request, redirect, url_for, make_response
 from flask.json import jsonify
@@ -24,35 +26,88 @@ authorization_endpoint = config['DEFAULT']['authorization_endpoint']
 token_endpoint = config['DEFAULT']['token_endpoint']
 userinfo_endpoint = config['DEFAULT']['userinfo_endpoint']
 redirect_uri = config['DEFAULT']['redirect_uri']
+base_uri = config['DEFAULT']['base_uri']
 jwt_secrets = config['DEFAULT']['jwt_secrets'].split(',')
 
 algorithm = 'HS256'
 jwt_cookie_key = 'jwt_token'
 
-
-def redirect_with_jwt(url, **jwt_args):
-    """
-    Redirect to url.
-    All keyword args will be signed into a JWT with the response.
-    """
-    resp = make_response(redirect(url))
-    resp.set_cookie(jwt_cookie_key, jwt.encode(
-        jwt_args,
-
-        # The first secret is the current signer.
-        jwt_secrets[0],
-        algorithm=algorithm
-    ))
-    return resp
+oauth_token = "oauth_token"
+oauth_state = "oauth_state"
+oauth_token_key = base_uri+'/'+oauth_token
+oauth_state_key = base_uri+'/'+oauth_state
 
 
-def get_jwt_token(cookies):
+def JWT(
+        oauth_token=None,
+        oauth_state=None,
+        sub=None,
+        iss=base_uri,
+        aud=base_uri,
+        exp_seconds=300,
+        **rest):
+    now = datetime.utcnow()
+    expires = now + timedelta(seconds=exp_seconds)
+    token = rest
+    if oauth_token:
+        token[oauth_token_key] = oauth_token
+    if oauth_state:
+        token[oauth_state_key] = oauth_state
+    if sub:
+        token['sub'] = sub
+    token['jti'] = str(uuid.uuid1())
+    token['iss'] = base_uri
+    token['aud'] = base_uri
+    token['exp'] = expires
+    token['iat'] = now
+    token['nbf'] = now
+    return token
+
+
+class Cookie:
+    def __init__(self):
+        self.keys = {}
+        self.ages = {}
+        self.secures = {}
+        self.deletes = []
+
+    def remove_jwt(self, key):
+        self.deletes.append(key)
+
+    def add_jwt(self, key, jwt, max_age=300, secure=True):
+        self.keys[key] = jwt
+        self.ages[key] = max_age
+        self.secures[key] = secure
+
+    def populate_resp(self, resp):
+        for key, token in self.keys.items():
+            resp.set_cookie(
+                key=key,
+                max_age=self.ages[key],
+                secure=self.secures[key],
+                value=jwt.encode(
+                    token,
+
+                    # The first secret is the current signer.
+                    jwt_secrets[0],
+                    algorithm=algorithm
+                )
+            )
+        for key in self.deletes:
+            resp.set_cookie(
+                key=key,
+                max_age=0
+            )
+        return resp
+
+
+def get_jwt_token(cookies, key):
     """
     Retrieve the jwt token's as a key, value object
     from the flask request cookies object.
     If no token exists, returns an empty object.
     """
-    if jwt_cookie_key in request.cookies:
+    if key in request.cookies:
 
         # We take multiple secrets to allow for online secret rotation.
         # The first secret is the current signer,
@@ -60,12 +115,21 @@ def get_jwt_token(cookies):
         # be rotated out.
         for jwt_secret in jwt_secrets:
             try:
+                # github.com/jpadilla/pyjwt/blob/master/tests/test_api_jwt.py
+                # This will validate:
+                #   iss, aud
+                # This will validate (if they are in the request):
+                #   exp, nbf, iat
                 return jwt.decode(
-                    request.cookies[jwt_cookie_key],
+                    request.cookies[key],
                     jwt_secret,
+                    issuer=base_uri,
+                    audience=base_uri,
                     algorithms=[algorithm]
                 )
-            except jwt.exceptions.InvalidTokenError:
+            except jwt.exceptions.InvalidTokenError as e:
+                # will catch exp, nbf, iat, iss, aud errors
+                print(e)
                 continue
 
     return {}
@@ -79,10 +143,10 @@ def index():
     Otherwise:
         the user is authenticated.
     """
-    jwt = get_jwt_token(request.cookies)
-    if 'oauth_token' in jwt:
+    jwt = get_jwt_token(request.cookies, oauth_token)
+    if oauth_token_key in jwt:
         # User is authenticated, don't do the auth dance.
-        client = OAuth2Session(client_id, token=jwt['oauth_token'])
+        client = OAuth2Session(client_id, token=jwt[oauth_token_key])
         return jsonify(client.get(userinfo_endpoint).json())
 
     client = OAuth2Session(
@@ -93,7 +157,10 @@ def index():
     authorization_url, state = client.authorization_url(authorization_endpoint)
 
     # State is used to prevent CSRF, keep this for later.
-    return redirect_with_jwt(authorization_url, oauth_state=state)
+    cookie = Cookie()
+    state_token = JWT(oauth_state=state)
+    cookie.add_jwt(oauth_state, state_token)
+    return cookie.populate_resp(make_response(redirect(authorization_url)))
 
 
 @app.route("/callback", methods=["GET"])
@@ -104,15 +171,15 @@ def callback():
     in the redirect URL. We will use that to obtain an access token.
     """
 
-    jwt = get_jwt_token(request.cookies)
-    if 'oauth_state' not in jwt:
+    jwt = get_jwt_token(request.cookies, oauth_state)
+    if oauth_state_key not in jwt:
         # something is wrong with the state token,
         # so redirect back to start over.
         return redirect(url_for('.index'))
 
     client = OAuth2Session(
         client_id,
-        state=jwt['oauth_state'],
+        state=jwt[oauth_state_key],
         redirect_uri=redirect_uri
     )
 
@@ -121,9 +188,21 @@ def callback():
         client_secret=client_secret,
         authorization_response=request.url
     )
+    userinfo = client.get(userinfo_endpoint).json()
 
     # Save the token
-    return redirect_with_jwt(url_for('.index'), oauth_token=token)
+    cookie = Cookie()
+    exp_seconds = 60*60*4
+    if "ExpiresIn" in token:
+        exp_seconds = token['ExpiresIn']
+    jwttoken = JWT(
+        exp_seconds=exp_seconds,
+        oauth_token=token,
+        **userinfo
+    )
+    cookie.add_jwt(oauth_token, jwttoken)
+    cookie.remove_jwt(oauth_state)
+    return cookie.populate_resp(make_response(redirect(url_for('.index'))))
 
 
 if __name__ == "__main__":
@@ -133,7 +212,7 @@ if __name__ == "__main__":
         host='0.0.0.0',
         port=443,
         ssl_context=(
-            'selfsign/output/localhost.pem',
-            'selfsign/output/key.pem'
+            'output/localhost.pem',
+            'output/key.pem'
         )
     )
